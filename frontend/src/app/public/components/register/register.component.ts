7298880a3fffc656f20a46eeb96866eda59982c8
Sergey Dziadevich <sergey.dziadevich@n-able.com>
2022-10-22 15:17:04 +0300

    added public and private modules to angular; added registration form;

--- frontend/src/app/public/components/register/register.component.ts
+++ frontend/src/app/public/components/register/register.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
+
+import {CustomValidators} from "../../_helpers/custom-validators";
+import { UserService } from "../../services/user.service";
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  form: FormGroup = new FormGroup(
+  {
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      username: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [Validators.required]),
+      passwordConfirm: new FormControl(null, [Validators.required]  )
+    },
+    { validators: CustomValidators.passwordsMatching }
+  );
 
-  ngOnInit(): void {
+  constructor(private userService: UserService, private router: Router) { }
+
+  get email(): FormControl {
+    return this.form.get('email') as FormControl;
+  }
+
+  get username(): FormControl {
+    return this.form.get('username') as FormControl;
+  }
+
+  get password(): FormControl {
+    return this.form.get('password') as FormControl;
+  }
+
+  get passwordConfirm(): FormControl {
+    return this.form.get('passwordConfirm') as FormControl;
+  }
+
+  register() {
+    if (this.form.valid) {
+      this.userService.create({
+        email: this.email.value,
+        password: this.password.value,
+        username: this.username.value,
+      }).pipe(tap(() => this.router.navigate(['../login'])))
+        .subscribe()
+    }
   }
 
 }
--- frontend/src/app/public/public.module.ts
+++ frontend/src/app/public/public.module.ts
@@ -4,16 +4,25 @@ import { CommonModule } from '@angular/common';
 import { PublicRoutingModule } from './public-routing.module';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
-
+import { MatCardModule } from "@angular/material/card";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
-    PublicRoutingModule
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    PublicRoutingModule,
   ]
 })
 export class PublicModule { }
