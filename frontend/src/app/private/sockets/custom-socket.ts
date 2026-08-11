38b8f25950cac25afd7b1251c175f86c79f08437
dweb <developersiteweb@gmail.com>
2026-08-11 09:25:09 +0200

    added dependabot

--- frontend/src/app/private/sockets/custom-socket.ts
+++ frontend/src/app/private/sockets/custom-socket.ts
@@ -1,30 +1,30 @@
-import { Socket, SocketIoConfig} from "ngx-socket-io";
+import { Socket, SocketIoConfig } from "ngx-socket-io";
 import { tokenGetter } from "../../app.module";
-import {Injectable} from "@angular/core";
+import { Injectable } from "@angular/core";
 
 @Injectable({providedIn: 'root'})
 export class CustomSocket extends Socket {
   constructor() {
-    const config: SocketIoConfig = { 
+    const config: SocketIoConfig = {
       url: 'http://localhost:3000',
       options: {
         extraHeaders: {
           Authorization: tokenGetter()
         }
-      } 
+      }
     };
     super(config);
   }
 
   reconnect() {
     this.disconnect();
-    const config: SocketIoConfig = { 
+    const config: SocketIoConfig = {
       url: 'http://localhost:3000',
       options: {
         extraHeaders: {
           Authorization: tokenGetter()
         }
-      } 
+      }
     };
     this.connect();
   }
